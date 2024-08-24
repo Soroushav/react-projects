e17fd3382624f7c3b9d9eb10158db2bd23561f6c
soroushav <soroushav85@gmail.com>
2024-08-24 13:47:43 +0330

    the wild oasis

--- 16.the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ 16.the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini"
 
 function LoginForm() {
-  const [email, setEmail] = useState("soroush@example.com");
-  const [password, setPassword] = useState("pass0987");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin({ email, password });
   function handleSubmit(e) {
     e.preventDefault();
